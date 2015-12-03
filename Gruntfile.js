dbb88576d102402e50d76bf64539307df8a6b523
Sid <sid.ghodke@gmail.com>
2015-12-03 12:34:11 -0800

    First pass to check if the required ports are busy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,8 @@
  */
 
 // 3rd party
-var easybuild = require('easy-build');
-
+var easybuild = require('easy-build'),
+    portscanner = require('portscanner');
 
 /* -----------------------------------------------------------------------------
  * easy-build
@@ -16,6 +16,18 @@ module.exports = function (grunt) {
 
   // loads library specific tasks
   grunt.loadTasks('build/tasks');
+
+  // Checks if ports 9997 to 9999 inclusive are available
+  portscanner.findAPortInUse(9997, 9999, '127.0.0.1', function(error, port) {
+    // Status is 'open' if currently in use or 'closed' if available
+    if (!error) {
+      if (port) {
+        throw new Error('Cannot start Services port ' + port + ' is being used.');
+      }
+    } else {
+      console.log('There was an error');
+    }
+  });
 
   // easy-build yo!
   easybuild.load(grunt, {
